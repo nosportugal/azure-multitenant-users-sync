3f1789287b20937e92022f8186800ad23a0e38c7
David Neves <david.neves@parceiros.nos.pt>
2021-10-26 12:17:47 +0100

    adds azure location terraform variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "subscription_id" {
   type        = string
   description = "The azure subscription id where the function will run."
 }
+
+variable "location" {
+  type        = string
+  description = "The resouces Azure location."
+}
